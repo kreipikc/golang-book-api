946116ea5b7e71555c18df06a0f11f04eeefb5f0
kreipikc <basmenov5kir@gmail.com>
2024-10-31 22:19:21 +0300

    small fix snitaxis

--- pkg/common/models/Book.go
+++ pkg/common/models/Book.go
@@ -4,7 +4,7 @@ import "gorm.io/gorm"
 
 type Book struct {
 	gorm.Model
-	Title       string `json: "title"`
-	Author      string `json: "author"`
-	Description string `json "description"`
+	Title       string `json:"title"`
+	Author      string `json:"author"`
+	Description string `json:"description"`
 }
